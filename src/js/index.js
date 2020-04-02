99b305806a3a97fcc4acf5649143903d5c78265f
Khalid Syfullah Zaman <khalid.z547@gmail.com>
2020-04-02 16:22:47 +0600

    Place ids for click to navigation scroll.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    
+    /* For sticky navigation */
     $('.js--section-features').waypoint(function(direction) {
         if(direction == "down"){
             $('nav').addClass('sticky');
@@ -8,4 +10,29 @@ $(document).ready(function() {
     }, {
         offset: '60px'
     });
+
+    /* Scroll on buttons */
+    // $('.js--scroll-to-plans').click(function(){
+    //     $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);
+    // });
+
+    // $('.js--scroll-to-start').click(function(){
+    //     $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
+    // });
+
+    /* navigation scroll */
+    // $(function() {
+    //     $('a[href*=#]:not([href=#])').click(function() {
+    //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    //         var target = $(this.hash);
+    //         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    //         if (target.length) {
+    //         $('html,body').animate({
+    //             scrollTop: target.offset().top
+    //             }, 1000);
+    //             return false;
+    //         }
+    //     }
+    //     });
+    // });
 })
